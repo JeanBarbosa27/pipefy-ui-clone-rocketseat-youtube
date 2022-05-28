f4c6f197ca418a169419fb49a7e03370d06672bc
Jean Barbosa <jeanbarbosa9@gmail.com>
2022-05-28 15:18:30 -0300

    Implements DnD Provider
    That enables React Drag and Drop handlers.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import GlobalStyle from './styles/global';
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+
+import GlobalStyle from "./styles/global";
+
+import Board from "./components/Board"
+import Header from "./components/Header";
 
 function App() {
   return (
-    <GlobalStyle />
+    <DndProvider backend={HTML5Backend}>
+      <Header />
+      <Board />
+      <GlobalStyle />
+    </DndProvider>
   );
 }
 
