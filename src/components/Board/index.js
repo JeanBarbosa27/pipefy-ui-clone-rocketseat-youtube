24e8eaf5096268b5e110888b980fd78153e68409
Jean Barbosa <jeanbarbosa9@gmail.com>
2022-05-28 15:20:15 -0300

    Implements board context

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,14 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import produce from "immer";
 
+import { loadLists } from "../../services/api";
+
+import BoardContext from "./context";
 import { Container } from "./styles";
-import Header from "../Header";
 import List from "../List";
 
 export default function Board() {
+  const data = loadLists();
+  const [lists, setLists] = useState(data);
+
+  function move(fromListIndex, toListIndex, fromCardIndex, toCardIndex) {
+    setLists(produce(lists, draft => {
+      const dragged = draft[fromListIndex].cards[fromCardIndex];
+      draft[fromListIndex].cards.splice(fromCardIndex, 1);
+      draft[toListIndex].cards.splice(toCardIndex, 0, dragged);
+    }))
+  }
+
   return (
-    <Container>
-      <Header />
-      <List />
-    </Container>
+    <BoardContext.Provider value={{ lists, move }}>
+      <Container>
+        {lists.map((list, index) => (
+          <List key={list.title} data={list} index={index} />
+        ))}
+      </Container>
+    </BoardContext.Provider>
   )
 }
