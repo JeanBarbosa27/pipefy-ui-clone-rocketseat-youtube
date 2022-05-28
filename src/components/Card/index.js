500212fea8f557e9b9389e02f3d5d9b8f2499d95
Jean Barbosa <jeanbarbosa9@gmail.com>
2022-05-28 15:20:33 -0300

    Creates card component

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,11 +1,75 @@
-import React from "react";
+import React, { useRef, useContext } from "react";
+import { useDrag, useDrop } from "react-dnd";
+import { MdInsertEmoticon } from "react-icons/md";
 
-import { Container } from "./styles";
+import BoardContext from "../Board/context";
 
-export default function Card() {
+import { Container, Label } from "./styles";
+
+export default function Card({ data, index, listIndex }) {
+  const ref = useRef();
+  const { move } = useContext(BoardContext);
+
+  const { labels, content, user: avatarUrl } = data;
+  const renderLabels = labels.map(label => (
+    <Label key={data.id} color={label} />
+  ))
+
+  const [{ isDragging }, dragRef] = useDrag({
+    type: "CARD",
+    item: { id: data.id, index, listIndex },
+    collect: monitor => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
+  const [_, dropRef] = useDrop({
+    accept: "CARD",
+    hover(item, monitor) {
+      const fromListIndex = item.listIndex;
+      const toListIndex = listIndex;
+      const draggedIndex = item.index;
+      const targetIndex = index;
+
+      if(fromListIndex === toListIndex && draggedIndex === targetIndex) {
+        return;
+      }
+
+      const targetSize = ref.current.getBoundingClientRect();
+      const targetVerticalCenter = (targetSize.bottom - targetSize.top) / 2;
+      const draggedOffset = monitor.getClientOffset();
+      const draggedTop = draggedOffset.y - targetSize.top;
+
+      if(draggedIndex < targetIndex && draggedTop < targetVerticalCenter) {
+        return;
+      }
+
+      if(draggedIndex > targetIndex && draggedTop > targetVerticalCenter) {
+        return;
+      }
+
+      move(fromListIndex, toListIndex, draggedIndex, targetIndex);
+
+      item.index = targetIndex;
+      item.listIndex = toListIndex;
+    }
+  });
+
+  dragRef(dropRef(ref));
+
   return (
-    <Container>
-      Card
+    <Container ref={ref} isDragging={isDragging}>
+      <header>
+        {renderLabels}
+      </header>
+      <p>{content}</p>
+      {avatarUrl && (
+        <img
+          src={avatarUrl}
+          alt="user avatar"
+          title="Nome do responsável pelo card"
+        />
+      )}
     </Container>
   )
 }
