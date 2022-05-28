cc6f488f0b4439368c181826c4fd4e0714008933
Jean Barbosa <jeanbarbosa9@gmail.com>
2022-05-28 15:21:15 -0300

    Implements list component

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,12 +1,31 @@
 import React from "react";
+import { MdAdd } from "react-icons/md";
 
 import { Container } from "./styles";
 import Card from "../Card";
 
-export default function List() {
+export default function List({data, index: listIndex}) {
+  const { title, cards, done, creatable } = data;
+
   return (
-    <Container>
-      <Card />
+    <Container done={done}>
+      <header>
+        <h2>{title}</h2>
+        {creatable && (
+          <button type="button">
+            <MdAdd size={24} color="#ffffff" />
+          </button>
+        )}
+      </header>
+      <ul>
+        {
+          cards.map((card, index) => (
+            <li key={card.id}>
+              <Card data={card} index={index} listIndex={listIndex} />
+            </li>
+          ))
+        }
+      </ul>
     </Container>
   )
 }
